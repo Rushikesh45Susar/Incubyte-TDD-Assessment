545dd4ff14d8d01471330469630aa8322e3b50b3
Rushikesh45Susar <rushikeshsusar45634@gmail.com>
2025-01-11 17:39:22 +0530

    feat: updated code to throw error when input have negative numbers

--- src/stringCalculator/StringCalculator.ts
+++ src/stringCalculator/StringCalculator.ts
@@ -2,6 +2,7 @@ function StringCalculator (numbers: string) {
     if(!numbers) return 0
     
     let delimeters:RegExp = new RegExp(',|\n')
+    let negativeNumbers:Number[] = []
 
     if(numbers.startsWith('//')) {
         const parts = numbers.split("\n", 2)
@@ -10,7 +11,15 @@ function StringCalculator (numbers: string) {
         numbers = parts[1]
     }
     
-    const numList = numbers.split(delimeters).map(i => parseInt(i, 10))
+    const numList = numbers.split(delimeters).map(i => {
+        const value = parseInt(i, 10)
+        if(value < 0) negativeNumbers.push(value)
+        return value
+    })
+
+    if(negativeNumbers.length > 0) {
+        throw new Error(`Negative numbers not allowed: ${negativeNumbers.join(", ")}`)
+    }
 
     return numList.reduce((sum, num) => sum + num, 0)
 }
