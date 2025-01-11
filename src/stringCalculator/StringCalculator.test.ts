8cf4da34f299254239f12f32164213082d50b210
Rushikesh45Susar <rushikeshsusar45634@gmail.com>
2025-01-11 16:49:53 +0530

    test: added test case to return number when provided numbers with comman separated

--- src/stringCalculator/StringCalculator.test.ts
+++ src/stringCalculator/StringCalculator.test.ts
@@ -12,4 +12,8 @@ describe('String Calculator', () => {
     test("should return the number for a multiple number input", () => {
         expect(add("123")).toBe(6);
     });
+
+    test("should return the number for multiple numbers separated by comma", () => {
+        expect(add("1,2,3")).toBe(6)
+    })
 })
