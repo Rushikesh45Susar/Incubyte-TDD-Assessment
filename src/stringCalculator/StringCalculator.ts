42057187f4439a1855357de6549e85026803ee41
Rushikesh45Susar <rushikeshsusar45634@gmail.com>
2025-01-11 17:32:50 +0530

    test: added support for custom delimeter

--- src/stringCalculator/StringCalculator.ts
+++ src/stringCalculator/StringCalculator.ts
@@ -1,7 +1,16 @@
 function StringCalculator (numbers: string) {
     if(!numbers) return 0
     
-    const numList = numbers.split(/,|\n/).map(i => parseInt(i, 10))
+    let delimeters:RegExp = new RegExp(',|\n')
+
+    if(numbers.startsWith('//')) {
+        const parts = numbers.split("\n", 2)
+        const customDelimiter = parts[0].substring(2)
+        delimeters = new RegExp(customDelimiter)
+        numbers = parts[1]
+    }
+    
+    const numList = numbers.split(delimeters).map(i => parseInt(i, 10))
 
     return numList.reduce((sum, num) => sum + num, 0)
 }
